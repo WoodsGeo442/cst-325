09f3fdad519184ff2b94f4471ec07b7d94b9fdb4
Geoffrey Woods <gewoods@csumb.edu>
2020-01-30 17:23:20 -0800

    initial commit

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,75 @@
+// NewSetUp.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    //add color
+    glClearColor(0.9, 0.3, 0.5, 1.0);
+
+    // init glew
+    glewExperimental = GL_TRUE;
+    glewInit();
+
+    // before the loop
+    float color_scale = 1.0;
+    float dt = 1.0 / 60.0;
+    float color_speed = 1;
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+
+        // in the loop
+        if (color_scale > 1.0) {
+            color_scale = 0.0;
+        }
+        else {
+            color_scale += color_speed * dt;
+        }
+        glClearColor(0.7 * color_scale, 0.0 * color_scale, 0.5 * color_scale, 1.0);
+    }
+
+
+    glfwTerminate();
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
