23dd058875196b33e873bd9e79dbe9696a8bc54e
WoodsGeo442 <gewoods@csumb.edu>
2020-02-04 14:55:44 -0800

    Initialize

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -1,11 +1,38 @@
 #define GLEW_STATIC
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include "Source.h"
 
 GLFWwindow* initialize_glfw() {
-	// ...start with the same init code as before...
+	// Initialize the context
+	if (!glfwInit()) {
+		std::cout << "glfwInit(...) failed\n";
+		abort();
+	}
+
+	// Ask for a core profile
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// Create the window
+	GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	if (!window) {
+		std::cout << "glfwCreateWindow(...) failed\n";
+		glfwTerminate();
+		abort();
+	}
+
+	// Make the context current
+	glfwMakeContextCurrent(window);
+
+	// Load the function pointers
+	glewExperimental = GL_TRUE;
+	glewInit();
+
 	return window;
 }
+
 
 void compile_shader() {} // TODO: implement later
 void load_geometry() {} // TODO: implement later
